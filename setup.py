27ba364a95d9cf9cead70e606e1e37389fc1a5b2
Canisback <canisback@gmail.com>
2019-03-04 18:24:49 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'roleml',
   packages = find_packages(),
-  version = '0.1.0',
+  version = '0.1.1',
   description = 'ML classifier for role in Riot API LoL matches',
   author = 'Canisback',
   author_email = 'canisback@gmail.com',
